68546bfbf9249ec7e3f9e8e79fdb98b9a2ff8bae
Saman Alishiri Shahrbabak <samanalishiri@gmail.com>
2019-03-05 16:24:38 +0330

    develop UI and improve script code

--- src/main/resources/static/js/client/client.js
+++ src/main/resources/static/js/client/client.js
@@ -38,16 +38,22 @@ function remove(id) {
 function bindModel(model) {
     document.getElementById("client_id").value = model.id;
     document.getElementById("client_name").value = model.name;
-    document.getElementById("redirect_url").value = model.redirectUrl;
-    document.getElementById("client_type").value = model.type;
+    document.getElementById("redirect_uri").value = model.redirectUrl;
+    document.getElementById("scopes").value = model.scopes;
 }
 
 function createClientModel() {
+    var scopes = [];
+    for (e of document.getElementsByName("scopes[]").values()) {
+        scopes.push(e.value);
+    }
+
     var model = {
         "id": document.getElementById("client_id").value,
         "name": document.getElementById("client_name").value,
-        "redirectUrl": document.getElementById("redirect_url").value,
+        "redirectUri": document.getElementById("redirect_uri").value,
         "type": document.getElementById("client_type").value,
+        "scopes": scopes,
     };
 
     return JSON.stringify(model);
@@ -58,7 +64,41 @@ function openDialog() {
 }
 
 function closeDialog() {
-    $("#dialog_client_form").modal('toggle');
+    $("#dialog_client_form").modal("toggle");
+}
+
+function selectScope(select) {
+    var option = select.options[select.selectedIndex];
+    var parent = document.getElementById("scope_elements");
+
+    var choices = parent.getElementsByTagName("input");
+    for (var i = 0; i < choices.length; i++) {
+        if (choices[i].value == option.value) {
+            return;
+        }
+    }
+
+    var row = document.createElement("div");
+    row.classList.add("bg-light");
+
+    var input = document.createElement("input");
+    input.type = "hidden";
+    input.name = "scopes[]";
+    input.value = option.value;
+    row.appendChild(input);
+
+    var text = document.createTextNode(option.firstChild.data);
+    row.appendChild(text);
+
+
+    var i = document.createElement("i");
+    i.classList.add("fa");
+    i.classList.add("fa-close");
+    i.classList.add("red");
+    i.setAttribute("onclick", "this.parentNode.parentNode.removeChild(this.parentNode);");
+    row.appendChild(i);
+
+    parent.appendChild(row);
 }
 
 
